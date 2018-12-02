72a94847e9f876e9963b8cb6d52c2026ff8ce0c5
Jorge <manzoj@oregonstate.edu>
2018-12-01 22:30:27 -0800

    😂

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
+
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+
+		<div id="addChild">
+			<input v-model="newMessage" placeholder="Text in new component">
+			<button v-on:click="makeNewComponent">Create</button>
+		</div>
+
+		<div id="appContainer">
+			<borked-app
+				v-for="child in children"
+				v-bind:key="child.id"
+				v-bind:msg="child.givenMsg"
+				v-on:remove-me="deleteChild(child.id)"
+			></borked-app>
+		</div>
+
   </div>
 </template>
 
 <script>
+import borkedApp from './borkedApp.vue'
 export default {
   name: 'app',
+	components: {
+		borkedApp
+	},
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App!'
+			children: [
+				{ id:1, givenMsg: "SCREAMING!!"},
+				{ id:2, givenMsg: "SCREAMING!!"},
+				{ id:3, givenMsg: "SCREAMING!!"}
+			],
+			newMessage: ''
     }
+  },
+  methods: {
+		makeNewComponent: function() {
+			this.children.push({ 'id': this.children.length + 1, 'givenMsg': this.newMessage})
+		},
+		deleteChild: function(id) {
+			for(let i = 0; i < this.children.length; i++){
+				if(this.children[i].id == id){
+					this.children.splice(i, 1)
+					break
+				}
+			}
+		}
   }
 }
 </script>
 
 <style lang="css">
-  #app {
-    color: #56b983;
+  #appContainer {
+    border: 3px solid grey;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
   }
 </style>
---
+++ src/borkedApp.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div id="borkedApp" 
+		:class="sizeClass" 
+		v-on:mouseover="makeMeBigger" 
+		v-on:mouseleave="makeDefault"
+		v-on:click="$emit('remove-me')"
+		>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'borkedApp',
+	props: ['msg'],
+  data () {
+    return {
+			sizeClass: 'defaultSize'			
+    }
+	},
+	methods: {
+		makeMeBigger: function(e) {
+			this.sizeClass = 'getBigger'
+		},
+		makeDefault: function(e) {
+			this.sizeClass = 'defaultSize'
+		}
+	}
+}
+</script>
+
+<style lang="css">
+  #borkedApp {
+		font-family: Arial, Helvetica, sans-serif;
+    color: white;
+		border: 3px solid #56b983;
+		border-radius: 10px;
+		margin: 0 1.5em 0 1.5em;
+		background-color: #264456;
+		transition: background-color 1s ease-out;
+  }
+	#borkedApp:hover {
+		background-color: #417799;
+  	cursor: pointer;
+	}
+	.defaultSize {
+		flex-grow: 1;
+	}
+	.getBigger {
+		flex-grow: 2;
+	}
+</style>
